c0d16097da1ffcd9943cd3062971af57ea84aebb
EgorDm <egordmitriev2@gmail.com>
2015-03-09 18:28:33 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>BlogEngine Demo Blog</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
